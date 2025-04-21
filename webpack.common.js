4db0e8fe17cc7a06c9e4753a89dfa62ec2c7934c
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2025-04-21 12:26:15 +0300

    Project init

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: './js/app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
